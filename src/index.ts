1dbcf1865fe7a1cecde02b10b8c657d62d98eec2
Surya Sikha Rej <46563058+iamsuryasikharej@users.noreply.github.com>
2019-11-07 19:32:18 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 
 import {apiservice} from "./apiservice";
 var x=new apiservice()
-// let y=x.gituserinfo("iamsuryasikharej");
+x.gituserinfo("iamsuryasikharej");
 x.getRepos("iamsuryasikharej")
-// console.log(y)
